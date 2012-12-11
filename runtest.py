54511ba201a38097375010ee8e7a38f9bf8e0c0a
hit9 <nz2324@126.com>
2012-12-11 20:37:54 +0800

    testcodes

--- runtest.py
+++ runtest.py
@@ -18,21 +18,45 @@ user = User()
 # insert 
 user.username = "hello"
 user.email = "email"
+user.save() # return 0 for failure, else return id
+
+# or like this:
+
+user = User(username = "hello", email = "email")
+
 user.save()
 
 #insert
 
 another_user = User.create(username = "hi", email = "yes")
 
-#select by primarykey
+#select 
 
-user3 = User.find(10)
+for user in User.select(): # select all, return a generator object
+    print user.id, user.email # etc
 
-if user3:
-    print "found!"
-    # update
-    user3.username = "en"
-    user3.save()
-else:
-    print "not found"
+# to select a few fields, but not all:
 
+User.select(User.username, User.id)
+
+# where
+
+for user in User.where((User.id >=  1)  & (User.email  ==  "hit9@hit9.org")).select():
+    print user.username
+
+# select a single record by primarykey
+
+user = User.get(10) #if not found, return None
+
+# delete
+
+User.where(User.id == 13).delete() # return 0 for failure, else return rownumber deleted
+
+#or like this:
+
+user.destroy() #return 0 or 1
+
+#update
+
+user.username = "updatename"
+user.save() #do a save on created object equal to update
