c4fc90cd30391c3ad8408cf5db5caf247b020a3f
hit9 <nz2324@126.com>
2012-12-13 17:59:17 +0800

    add api: Database.query_times # record times executed SQL
    add argument to Database.config(**configs).argument:debug,default as
    False
    
    add Empty Exception Class SQLSyntaxError,will pirnt SQL when An Syntax
    Error occured under debug pattern
    
    add Class SelectResult to deal with query result of select
    
    remove get() from Model class.do this with`
    Model.at(myidvalue).select().fetchone() `instead
    
    expressions edit
    
    improve expressions list and data dict use

--- virgo.py
+++ virgo.py
@@ -25,6 +25,14 @@ vg_cursor_matched_re = re.compile(r'Rows matched: (\d+)')
 
 
 class Database:
+    
+    #
+    # api:
+    # None Database.config(**configs)
+    # cursor Database.execute(sql)
+    # int Database.query_times  # count for query times
+
+
     """class to manage Database connection"""
 
     configs = { # default configs for connection
@@ -38,9 +46,14 @@ class Database:
 
     conn = None # connection object of MySQLdb
 
+    debug = True # if under debug
+
+    query_times = 0
+
     @classmethod
-    def config(cls, **configs):
+    def config(cls, debug = True, **configs):
         cls.configs.update(configs)
+        cls.debug = debug
 
     @classmethod
     def connect(cls):
@@ -51,8 +64,12 @@ class Database:
     @classmethod
     def execute(cls, sql): # execute SQL
         cursor = Database.connect().cursor()
-        cursor.execute(sql)
-
+        try:
+            cursor.execute(sql)
+        except: # report SQL be made
+            if cls.debug:
+                raise SQLSyntaxError, sql
+        cls.query_times = cls.query_times+1
         # add attribute 're' to cursor:store query matched rows number
         cursor.re = int(vg_cursor_matched_re.search(cursor._info).group(1)) if cursor._info else cursor.rowcount
         return cursor
@@ -61,24 +78,64 @@ class Database:
     # cursor.lastrowid  => record primarykey value of row last insert 
 
 
+class SQLSyntaxError(Exception):pass
+
 class Expr(object):
 
-    def __init__(self, expstr):
-        self.expstr = expstr
+    #
+    # api:
+    # str expr._tostr
+    #
 
 
+    def __init__(self, left, right, op, exprstr = None):
+        self.left = left # the left field
+        self.right = right # the right , field or string
+        self.op = op # the operator string
+        self.exprstr = exprstr
+
+    
+    @property
+    def _tostr(self): # singleton get exprstr
+        if not self.exprstr:
+            self.exprstr =  self.left.fullname+self.op+self._str(self.right)
+        return self.exprstr
+
+    def _str(self, side) :# turn some side to str in SQL
+        if isinstance(side, Field):
+            return side.fullname
+        else:
+            return "'"+MySQLdb.escape_string(str(side))+"'" # escape_string
+
+    
     def _expr(op):
-        def e(self, r):
-            if isinstance(r, Expr): # if r is a expr
-                self.expstr = self.expstr+" "+op+" "+r.expstr
+        def e(self, r_expr): # return expr instance
+            if isinstance(r_expr, Expr):#if r_expr is Expr instance
+                self.exprstr =  self._tostr+" "+op+" "+r_expr._tostr
                 return self
-            return False
+            return None
         return e
 
     __and__ = _expr("and")
 
     __or__ = _expr("or")
 
+
+class EqExpr(Expr): # eq expr
+    #
+    # api:
+    # (fieldname, valuestring) eqexpr._toitem 
+    #
+    
+    def __init__(self, left, right, exprstr = None):
+        self.op = " = "
+        self.left = left
+        self.right = right
+        self.exprstr = None
+
+    @property
+    def _toitem(self): # User.name == "name" => ("name", "name")
+        return (self.left.name, str(self.right))
 
 
 class FieldDescriptor(object): # descriptor for Field objs
@@ -95,11 +152,10 @@ class FieldDescriptor(object): # descriptor for Field objs
     def __set__(self, instance, value):
         instance._data[self.name] = value
 
-    
 
 class Field(object): # Field Object
 
-    name = None 
+    name = None
     model = None # Model Class
     primarykey = False # if primarykey
 
@@ -113,15 +169,12 @@ class Field(object): # Field Object
         return self.model._info.table_name+"."+self.name
 
     def _expr(op): # function generator
-        def e(self, r):
-            if isinstance(r, Field):
-                s = r.fullname
-            else :
-                s = "'"+MySQLdb.escape_string(str(r))+"'" # escape_string 
-            return Expr(self.fullname+op+s)
+        def e(self, r): # return Expr instance
+            return Expr(left = self, right = r, op = op)
         return e
 
-    __eq__ = _expr(" = ")
+    def __eq__(self, r):
+        return EqExpr(left = self, right = r)
 
     __lt__ = _expr(" < ")
 
@@ -131,8 +184,8 @@ class Field(object): # Field Object
 
     __ge__ = _expr(" >= ")
 
-    __ne__ = _expr(" <> ") 
-    
+    __ne__ = _expr(" <> ")
+
 
 class PrimaryKey(Field):
 
@@ -140,115 +193,133 @@ class PrimaryKey(Field):
         self.primarykey = True
 
 
-class QueryRuntime(object): # store Single SQL Runtime information
-    
-    def __init__(self, table_name, primarykey):
-        self.table = table_name
-        self.primarykey = primarykey # primarykey instance
-        self.ks = (
-            "where",  # runtime.where  => expr 
-            "set",  # set  => expr  lst
-            "orderby", # (field, desc) tuple , desc => bool
-            "select" # fields lst to select from db
-        )
-        self.reset()
-    
+class SelectResult(object): # wrap select result
 
-    def reset(self):
-        self.__dict__.update({}.fromkeys(self.ks, None))
+    def __init__(self, model):
+        self.model = model
+        self.cursor = None
+
+
+    def fetchall(self):# fetchall result
+        for dct in self.cursor.fetchall():
+            yield self.model(**dct)
+
+    def fetchone(self):# fetchone a time
+        dct = self.cursor.fetchone()
+        return self.model(**dct) if dct else None
 
     @property
-    def _orderby(self):
-        restr = ""
-        a = self.orderby
-        if a:
-            restr =  " order by "+a[0].fullname
-            if a[1]:
-                restr = restr+" desc "
-        return restr
+    def to_tuple(self):
+        return self.cursor.fetchall()
+
+
+class Query(object):
+
+    def __init__(self, model = None):
+
+        self.model = model
+        self.runtime = ( # record infomation in single query,
+                "_where", 
+                "_set", 
+                "_orderby", 
+                "_select" 
+                )
+        self.reset_runtime()
+        self.select_result = SelectResult(model)
+
+
+    def reset_runtime(self):
+        self.__dict__.update({}.fromkeys(self.runtime, ""))
+
 
     @property
-    def _set(self): # make set string
-        lst = self.set
-        if lst:
-            return " set "+", ".join([expr.expstr for expr in lst])
-        return ""
+    def _primarykey(self): # primarykey
+        return self.model._info.primarykey
 
     @property
-    def _select(self):#make select fields str
-        flst = self.select
+    def _table(self):
+        return self.model._info.table_name
+
+    def set_orderby(self, t): # t => (Field instance, True of False)
+        s = " order by "+t[0].fullname
+        if t[1]:
+            s = s+" desc "
+        self._orderby = s
+
+    def set_select(self, flst): # flst => Field instance list
         if flst:
             fnlst = []
             pk = None
             for f in flst:
-                if f is self.primarykey:
+                if f is self._primarykey:
                     pk = f
                 fnlst.append(f.name)
             if not pk:#if no primarykey in flst
-                fnlst.append(self.primarykey.name)
+                fnlst.append(self._primarykey.name)
 
             fstr = ", ".join(fnlst)
         else:
             fstr = "*"
-        return fstr
+        self._select = fstr
 
-    @property
-    def _where(self):
-        if self.where:
-            return  " where "+self.where.expstr
-        return ""
+    def set_where(self, lst, dct):
+        lst = list(lst)
+        fields = self.model._info.fields
+        lst.extend([fields[k] == v for k, v in dct.iteritems()])
+        self._where = " where "+" and ".join([expr._tostr for expr in lst])
+
+
+    def set_set(self, lst, dct):
+        lst = list(lst) # cast to list type
+        fields = self.model._info.fields
+        primarykey = self._primarykey
+        lst.extend([fields[k] == v for k, v in dct.iteritems() if fields[k] is not primarykey])
+        self._set = " set " + ", ".join([expr._tostr for expr in lst])
+ 
+    def _G(name, default):
+        @property
+        def g(self):
+            if self.__dict__[name]:
+                return self.__dict__[name]
+            return default
+        return g
+
+    get_orderby = _G("_orderby", "")
+
+    get_select = _G("_select", "*")
+
+    get_set = _G("_set", "")
+
+    get_where = _G("_where", "")
 
     def makeSQL(self, type = None):
+        table = self._table
         if type == 1: #insert
-            SQL = "insert into "+self.table+" "+self._set
+            SQL = "insert into "+table+" "+self.get_set
         elif type == 2:#update
-            SQL = "update "+self.table+" "+self._set+self._where
+            SQL = "update "+table+" "+self.get_set+self.get_where
         elif type == 3:#select
-            SQL = "select "+self._select+" from "+self.table+self._where+self._orderby
+            SQL = "select "+self.get_select+" from "+table+self.get_where+self.get_orderby
         elif type == 4:#delete
-            SQL = "delete from "+self.table+self._where
-        return SQL
-    
+            SQL = "delete from "+table+self.get_where
+        return SQL   
 
-class Query(object): # one Model  => one Query instance
-
-    def __init__(self, model):
-        self.model = model 
-        self.runtime = QueryRuntime(model._info.table_name, model._info.primarykey) # make  an empty runtime
-
-    def set_set(self, dct): # set_set by data dct
-        lst = []
-        model = self.model
-        for f in model._info.fields:
-            if f.name in dct.keys() and f is not model._info.primarykey:
-                lst.append(f == dct[f.name])
-        self.runtime.set = lst
-
-
-    def set_where(self, expr):
-        self.runtime.where = expr
-
-    def set_orderby(self, field):
-        self.runtime.orderby = field
-
-    def set_select(self, fields):
-        self.runtime.select = fields
-
-    def _Q(type):  # function generator for CURD
+    def _Q(type): # function generator for CURD
         def func(self):
             re = None
-            SQL = self.runtime.makeSQL(type = type)
+            SQL = self.makeSQL(type = type)
             cursor = Database.execute(SQL)
             if type in (2, 4):# if update or delete, return success or failure
                 re = cursor.re
             elif type is 1:# if insert , return lastrowid
-                re = cursor.lastrowid if  cursor.re else None
+                re = cursor.lastrowid if cursor.re else None
             elif type is 3:# if select
-                re = cursor # return cursor obj
-            self.runtime.reset() # reset runtime
+                self.select_result.cursor = cursor # reset select_result's cursor
+                re = self.select_result
+            self.reset_runtime() # reset runtime
             return re
         return func
-        
+
     #generate CURD Functions
 
     insert = _Q(1)
@@ -260,87 +331,95 @@ class Query(object): # one Model  => one Query instance
     delete = _Q(4)
 
 
+class ModelInfo(object): # one Model => one ModelInfo instance .store info of Model Class
 
-class ModelInfo(object): # one Model => one  ModelInfo instance .store info of Model Class
-
-    def __init__(self, 
+    def __init__(self,
             table_name = None, # Model class 's table name
             primarykey = None, # PrimaryKey instance
-            fields  = None     # fields lst
+            fields = None # dict {name:field}
             ):
         self.table_name = table_name
         self.primarykey = primarykey
-        self.fields = fields
-
+        self.fields = fields # name fields pairs dict
 
 
 class MetaModel(type):
 
     def __init__(cls, name, bases , attrs):
 
-        table_name = cls.__name__.lower()  
-        fields = []  # list of fields
-        primarykey = None 
-        
+        table_name = cls.__name__.lower()
+        fields = {} # name <=> fields pairs
+        primarykey = None
+
         for name, attr in cls.__dict__.iteritems():
             if isinstance(attr, Field):
                 attr.describe(name, cls) # describe attr
-                fields.append(attr)
+                fields[name] = attr
                 if attr.primarykey:
                     primarykey = attr
 
         if primarykey is None: # default primarykey is id
             primarykey = PrimaryKey()
             primarykey.describe('id', cls)
-            fields.append(primarykey)
+            fields['id'] = primarykey
 
         # set attributes for Model Class
         cls._info = ModelInfo(
-                table_name = table_name, 
-                primarykey = primarykey, 
+                table_name = table_name,
+                primarykey = primarykey,
                 fields = fields
-                )       
+                )
 
-        cls._query = Query(cls) # instance a Query for Model cls
+        cls.query = Query(cls) # instance a Query for Model cls
+
+    @property
+    def _primarykey(self):
+        return self._info.primarykey
 
 
 class Model(object):
 
     __metaclass__ = MetaModel
 
-    def __init__(self, **data):
-        lst = [f.name for f in self._info.fields] # list of field's name
-        self._data = {}.fromkeys(lst, None)
-        self._data.update(data)
+
+    def __init__(self, *lst, **dct):
+        fields = self._info.fields
+        self._data = {}.fromkeys(fields.keys(), None) # init self._data
+        self._data.update(dict(x._toitem for x in lst)) # update _data by expr lst
+        self._data.update(dct)
         self._cache = self._data.copy()
 
     @property
-    def _primarykey(self):
-        return self._info.primarykey.name
+    def to_dict(self):
+        return self._data
 
     @property
-    def _id(self): # the primarykey marks one object
-        return self._data[self._primarykey]
+    def _primarykey(self):
+        return self._info.primarykey
+
+    @property 
+    def _id(self):
+        return self._data[self._primarykey.name]
 
     def set_id(self, _id):
-        self._data[self._primarykey] = _id
+        self._data[self._primarykey.name] = _id
 
     def save(self):
         model = self.__class__
         if not self._id:
-            model._query.set_set(self._data)
-            _id = model._query.insert()
-            if _id:
-                self.set_id(_id)
+            model.query.set_set([], self._data)
+            id = model.query.insert()
+            if id:
+                self.set_id(id)
                 self._cache = self._data.copy() # sync cache after save
-                return _id # success insert, return the insert record's id
+                return id
         else:
-            model._query.set_where(model._info.primarykey ==  self._id)
+            model.where(model._primarykey == self._id)
             dct = dict(set(self._data.items()) - set(self._cache.items())) # only update changed data
             if not dct:
                 return 1 # data not change
-            model._query.set_set(dct)
-            re = model._query.update()
+            model.query.set_set([], dct)
+            re = model.query.update()
             if re:
                 self._cache = self._data.copy() # sync cache after save
                 return re #success insert
@@ -349,43 +428,44 @@ class Model(object):
     def destroy(self):
         if self._id:
             model = self.__class__
-            model._query.set_where(model._info.primarykey == self._id)
-            return model._query.delete()
+            model.where(model._info.primarykey == self._id)
+            return model.query.delete()
         return 0
 
     @classmethod
-    def create(cls, **data):
-        cls._query.set_set(data)
-        id = cls._query.insert() # insert return id
+    def create(cls, *lst, **dct):
+        cls.query.set_set(lst, dct)
+        id = cls.query.insert()
         if id:
-            data[cls._info.primarykey.name] = id # add id to data dct
-            return cls(**data)
+            dct[cls._primarykey.name] = id  # add id to dct
+            return cls(*lst, **dct)
         return None
 
     @classmethod
-    def where(cls, expr):
-        cls._query.set_where(expr)
+    def where(cls, *lst, **dct):
+        cls.query.set_where(lst, dct)
+        return cls
+
+    @classmethod
+    def at(cls, _id):# find by id
+        cls.where(cls._primarykey == _id)
         return cls
 
     @classmethod
     def orderby(cls, field, desc = False):
-        cls._query.set_orderby((field, desc))
+        cls.query.set_orderby((field, desc))
         return cls
 
     @classmethod
-    def select(cls, *flst):
-        cls._query.set_select(flst)
-        cur = cls._query.select()
-        for dct in cur.fetchall():
-            yield cls(**dct)
-
-    @classmethod
-    def get(cls, key): # select by primarykey
-        cls._query.set_where(cls._info.primarykey == key)
-        cur = cls._query.select()
-        dct = cur.fetchone()
-        return cls(**dct) if dct else None # if find, return it, else return None
+    def select(cls, *flst): 
+        cls.query.set_select(flst)
+        return cls.query.select()
 
     @classmethod
     def delete(cls):
-        return cls._query.delete()
+        return cls.query.delete()
+
+    @classmethod
+    def update(cls, *lst, **dct):
+        cls.query.set_set(lst, dct)
+        return  cls.query.update()
