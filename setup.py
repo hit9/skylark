d776bf704e067eca731abcde8a249eb92248f902
hit9 <nz2324@126.com>
2012-12-15 18:09:21 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "virgo",
+    version = "0.1",
+    author = "hit9",
+    author_email = "nz2324@126.com",
+    description = ("Simple & Easy to use ORM for Python & MySQL"),
+    license = "BSD",
+    keywords = "ORM MySQL Python database virgo",
+    url = "https://github.com/hit9/virgo",
+    py_modules=['virgo'],
+    install_requires = ['MySQL-python'],
+)
