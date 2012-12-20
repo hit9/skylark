3e129dfc3d8a342bc2df5824ad33a031f9858d54
hit9 <nz2324@126.com>
2012-12-20 21:27:38 +0800

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "virgo",
-    version = "0.1",
+    version = "0.1.1",
     author = "hit9",
     author_email = "nz2324@126.com",
     description = ("Simple & Easy to use ORM for Python & MySQL"),
