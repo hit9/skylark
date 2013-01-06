b6b3c71fb996cd450b7d711b47fa0a9b5d4df2b3
hit9 <nz2324@126.com>
2013-01-06 15:28:20 +0800

    engine=innodb

--- tests/tables.sql
+++ tests/tables.sql
@@ -23,9 +23,10 @@ create table user(
     id int primary key auto_increment, 
     name varchar(33), 
     email varchar(33)
-); 
+) engine=InnoDB; 
 create table post(
     post_id int primary key auto_increment,
     name varchar(100), 
     user_id int
-); 
+) engine=InnoDB; 
+
