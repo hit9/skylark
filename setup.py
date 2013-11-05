347996c29247df9d7bf94f61183d77be356419d3
hit9 <nz2324@126.com>
2013-11-05 15:22:31 +0800

    release 0.2.5

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="CURD.py",
-    version="0.2.4",
-    author="hit9",
-    author_email="nz2324@126.com",
-    description=("Tiny Python ORM for MySQL"),
-    license="BSD",
-    keywords="CURD ORM MySQL Python tiny database",
-    url="https://github.com/hit9/CURD.py",
+    name='CURD.py',
+    version='0.2.5',
+    author='hit9',
+    author_email='nz2324@126.com',
+    description=('Tiny Python ORM for MySQL'),
+    license='BSD',
+    keywords='CURD ORM MySQL Python tiny database',
+    url='https://github.com/hit9/CURD.py',
     py_modules=['CURD'],
     install_requires = ['MySQL-python'],
 )
