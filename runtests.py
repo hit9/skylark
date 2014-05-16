66c648563fabd3b334fc4c1ac3dcb5ba94abb58a
hit9 <nz2324@126.com>
2014-05-17 00:01:10 +0800

    exit with non-zero once any testprocess broken

--- runtests.py
+++ runtests.py
@@ -8,7 +8,8 @@ if sys.hexversion < 0x03000000:
 else:
     PY_VERSION = 3
 
-base_cmd = 'DBAPI=%s NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture --process-timeout=4;'
+base_cmd = ('DBAPI=%s NOSE_NOCAPTURE=1 '
+            'nosetests -w tests -x -v --nologcapture --process-timeout=4;')
 
 cmds = [base_cmd % 'pymysql', base_cmd % 'sqlite3']
 
@@ -16,4 +17,7 @@ if PY_VERSION == 2:
     cmds.insert(0, base_cmd % 'MySQLdb')
 
 for cmd in cmds:
-    call(cmd, shell=True)
+    exit_code = call(cmd, shell=True)
+
+    if exit_code != 0:
+        sys.exit(exit_code)
