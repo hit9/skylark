1a7d4b618678efe2a63e7427f4a7216e2756c05a
hit9 <nz2324@126.com>
2014-05-16 21:32:48 +0800

    add timeout limit: 4s

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DBAPI=MySQLdb NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture
-DBAPI=pymysql NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture
-DBAPI=sqlite3 NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture
+DBAPI=MySQLdb NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture --process-timeout=4;
+DBAPI=pymysql NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture --process-timeout=4;
+DBAPI=sqlite3 NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture --process-timeout=4;
