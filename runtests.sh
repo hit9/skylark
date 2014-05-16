bd3f115edf2f79ca5829c1c9fe9a0d9ec62fdcdd
hit9 <nz2324@126.com>
2014-05-16 23:51:53 +0800

    add local venvpy3 to gitignore list

--- runtests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-[ -z "$TRAVIS_PYTHON_VERSION" ] && TRAVIS_PYTHON_VERSION='2.7';
-
-if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]] || [[ $TRAVIS_PYTHON_VERSION == '2.6' ]]; then
-  DBAPI=MySQLdb NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture --process-timeout=4;
-fi;
-DBAPI=pymysql NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture --process-timeout=4;
-DBAPI=sqlite3 NOSE_NOCAPTURE=1 nosetests -w tests -x -v --nologcapture --process-timeout=4;
